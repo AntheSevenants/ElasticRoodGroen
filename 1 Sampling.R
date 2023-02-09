d9b2404b5db5ea0b9d2e5be39a6314d58e9adf1d
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-02-09 16:35:09 +0100

    Implement frequency-agnostic sampling

--- 1 Sampling.R
+++ 1 Sampling.R
@@ -45,17 +45,29 @@ crosstable
 SAMPLE_PER_COMPONENT = 1000
 
 # We keep a record of which components are eligible to be used
+# We now know each participle has enough attestations, but are there enough
+# participles in the first place?
 component_whitelist <- c()
 # Loop over each component
 for (component in dimnames(crosstable)$component) {
-  # Keep the frequencies of each component and each order
+  # For each order, check how many participles are actually there
   frequencies <- c()
   for (order in dimnames(crosstable)$order) {
-    frequencies <- append(frequencies, crosstable[order, component])
+    # Filter the data frame for that component and order
+    df_component <- df_filtered[df_filtered$component == component &
+                                df_filtered$order == order,]
+    
+    # Get all unique participles
+    eligible_participles <- unique(df_component$participle)
+    
+    frequencies <- append(frequencies, length(eligible_participles))
   }
   
+  # Find the required number of participles
+  n_participles_to_sample <- SAMPLE_PER_COMPONENT / MINIMUM_PARTICIPLE_THRESHOLD
+  
   # If the minimum frequency is enough, add the component to our whitelist
-  if (min(frequencies) >= SAMPLE_PER_COMPONENT) {
+  if (min(frequencies) >= n_participles_to_sample) {
     component_whitelist <- append(component_whitelist, component)
   }
 }
@@ -69,11 +81,41 @@ for (component in component_whitelist) {
   for (order in dimnames(crosstable)$order) {
     # Filter the data frame for that component and order
     df_component <- df_filtered[df_filtered$component == component & df_filtered$order == order,]
-    # Sample the required rows
-    df_component_sample <- df_component[sample(nrow(df_component),
-                                               size = SAMPLE_PER_COMPONENT,
-                                               replace = FALSE),]
-    # Append to list of samples
+    
+    # Get all unique participles
+    eligible_participles <- unique(df_component$participle)
+    
+    # Find the required number of participles
+    n_participles_to_sample <- SAMPLE_PER_COMPONENT / MINIMUM_PARTICIPLE_THRESHOLD
+    
+    # Actually sample the participles
+    sampled_participles <- sample(eligible_participles,
+                                  n_participles_to_sample, replace=FALSE)
+    
+    # This list will hold all sampled items from the different participles
+    participle_samples <- c()
+    
+    # For each participle, sample the minimum participle threshold
+    for (participle in sampled_participles) {
+      # Only get the rows corresponding to this participle
+      df_participle <- df_component[df_component$participle == participle,]
+      
+      #print(participle)
+      #print(nrow(df_participle))
+      #flush.console()
+      
+      # Sample the required rows
+      df_participle_sample <- df_participle[sample(nrow(df_participle),
+                                           size = MINIMUM_PARTICIPLE_THRESHOLD,
+                                           replace = FALSE),]
+      
+      participle_samples <- append(participle_samples, list(df_participle_sample))
+    }
+    
+    # Combine all participle samples
+    df_component_sample <- do.call("rbind", participle_samples)
+  
+    # Append to list of component samples
     samples <- append(samples, list(df_component_sample))
   }
 }
