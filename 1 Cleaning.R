994c0875a24162b6ace2249283a0b7900e3a862b
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-02-22 16:49:13 +0100

    Do more data cleaning first

---
+++ 1 Cleaning.R
@@ -0,0 +1,46 @@
+# Import the dt fixing library
+source("naive-dt-fix/naive-dt-fix.R")
+
+# Read dataframe
+df <- read.csv("RoodGroenAnthe.csv")
+
+# Find the original items count
+original_items_count <- nrow(df)
+
+# Fix tokenisation errors
+df$participle <- gsub(".»", "", df$participle)
+df$participle <- gsub("»", "", df$participle)
+df$participle <- tolower(df$participle)
+
+# Fix spelling errors
+df$participle <- gsub("gedomicileerd", "gedomicilieerd", df$participle)
+df$participle <- gsub("gedouched", "gedoucht", df$participle)
+
+# Automatic dt fixing
+df <- fix_participle_dt(df, "participle")
+
+# Fix random things
+df$participle <- gsub("gedefiniëerd", "gedefinieerd", df$participle)
+
+# Remove all participles ending in 'e'
+# These participles are declensed, and are thus ADJECTIVES!
+get_last_char <- function(value) {
+  return(substr(value, nchar(value), nchar(value)) )
+}
+get_last_char <- Vectorize(get_last_char)
+
+df <- df[get_last_char(df$participle) != "e", ]
+
+# Build a frequency table so we know the participle counts
+participle_counts <- table(df$participle)
+
+# Remove all hapaxes
+df <- subset(df,
+        participle %in% 
+        names(participle_counts[participle_counts > 1]))
+
+# Compute the difference
+difference <- original_items_count - nrow(df)
+print(difference)
+
+write.csv(df, "output/RoodGroenAnthe_sampled.csv", row.names = FALSE)
