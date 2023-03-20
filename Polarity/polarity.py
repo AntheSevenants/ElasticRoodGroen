48793f551359eac6139a861bc5baa50eb65d0752
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-20 10:35:39 +0100

    Enable output

--- Polarity/polarity.py
+++ Polarity/polarity.py
@@ -14,14 +14,25 @@ model = AutoModelForSequenceClassification.from_pretrained("DTAI-KULeuven/robber
 
 df = pd.read_csv("../RoodGroenAnthe.csv")
 
+#df = df.head(1000)
+
 CHUNK_SIZE = 100
 
 chunks = divide_chunks(list(df["sentence"]), CHUNK_SIZE)
 total_chunks = ceil(len(df["sentence"]) / CHUNK_SIZE)
 #print(f"Split {len(df['sentence'])} sentences into {len(chunks)} chunks")
 
+output_logits = []
+
 for chunk in tqdm(chunks, total=total_chunks):
     inputs = tokenizer(chunk, return_tensors='pt', padding=True)
     output = model(**inputs)
     # Convert to regular floats
     output = output.logits.softmax(dim=-1).detach().tolist()
+
+    output_logits += output
+
+df["negative"] = list(map(lambda polarity: polarity[0], output_logits))
+df["posistive"] = list(map(lambda polarity: polarity[1], output_logits))
+
+df.to_csv("RoodGroenAnthePolarity.csv", index=False)
