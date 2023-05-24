8d9d204aa08a531cf8920b92988e53d7ce0ff349
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-05-24 15:53:52 +0200

    Add basic distributional coordinates visualisation

---
+++ 94 Visualisation basic.R
@@ -0,0 +1,21 @@
+df <-
+  read.csv("output/RoodGroenAnthe_coefficients_infused_vectors.csv")
+df <- df[df$coefficient != 0,]
+df$order <- ifelse(df$coefficient > 0, "red", "green")
+
+colours = c("green", "red")
+
+plot_basic_coords <- function(technique) {
+  x_column <- paste0(technique, ".all.x")
+  y_column <- paste0(technique, ".all.y")
+  
+  built_plot <- plot(df[,x_column],
+                     df[,y_column],
+                     ylim=c(-2.5, 2.5),
+                     pch = 19,
+                     cex = 0.5,
+                     col=colours[factor(df$order)],
+                     xlab="", ylab="")
+  
+  return(built_plot)
+}
