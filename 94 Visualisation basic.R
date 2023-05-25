3c4376c9620c82e5b4e10851ec63d4548a7b1506
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-05-25 12:13:21 +0200

    Add legend and adapt margins

--- 94 Visualisation basic.R
+++ 94 Visualisation basic.R
@@ -4,22 +4,34 @@ df <-
 
 df$order <- ifelse(df$coefficient > 0, "red", "green")
 df$order <- ifelse(df$coefficient == 0, "removed", df$order)
+df$order <- factor(df$order, levels=c("red", "green", "removed"))
 
-colours = c("green", "red", "gray")
+colours = c("red", "green", "gray")
 
-plot_basic_coords <- function(technique) {
+plot_basic_coords <- function(technique, legend=FALSE) {
   x_column <- paste0(technique, ".all.x")
   y_column <- paste0(technique, ".all.y")
+  
+  par(mar=c(2, 2, 1, 1))
   
   plot(
     df[, x_column],
     df[, y_column],
-    ylim = c(-2.5, 2.5),
     pch = 19,
     cex = 0.5,
     col = colours[factor(df$order)],
     xlab = "",
     ylab = "",
-    asp = 1
+    asp = 1,
+    mar = c(1, 1, 1, 1)
   )
+  
+  if (legend) {
+    legend("bottomright",
+           legend = levels(df$order),
+           pch = 19,
+           col = colours,
+           bty = "n",
+           horiz = FALSE)
+  }
 }
