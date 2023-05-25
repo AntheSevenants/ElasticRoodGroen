aee22d85c97e9e4b4a8f38586bda9cfebc588a4a
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-05-25 10:56:37 +0200

    Also include removed coefficients in plot

--- 94 Visualisation basic.R
+++ 94 Visualisation basic.R
@@ -1,9 +1,11 @@
 df <-
   read.csv("output/RoodGroenAnthe_coefficients_infused_vectors.csv")
-df <- df[df$coefficient != 0,]
-df$order <- ifelse(df$coefficient > 0, "red", "green")
+#df <- df[df$coefficient != 0,]
 
-colours = c("green", "red")
+df$order <- ifelse(df$coefficient > 0, "red", "green")
+df$order <- ifelse(df$coefficient == 0, "removed", df$order)
+
+colours = c("green", "red", "gray")
 
 plot_basic_coords <- function(technique) {
   x_column <- paste0(technique, ".all.x")
@@ -21,5 +23,3 @@ plot_basic_coords <- function(technique) {
     asp = 1
   )
 }
-
-plot_basic_coords("tsne")
