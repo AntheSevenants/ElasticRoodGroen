79381d33728851d80c244a518b44503fdaf10bbc
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-06-27 15:49:06 +0200

    Also add too.far to export dataset

--- 2-4 GAM dataset.R
+++ 2-4 GAM dataset.R
@@ -7,7 +7,7 @@ for (technique in c("mds", "tsne", "umap")) {
   for (kind in c("all", "non_zero", "outside_sd")) {
     target_col <- paste0(technique, ".", kind)
     fit <- build_gam(df, technique, kind)
-    pred_df <- get_predictions_df(df, fit, technique, kind)
+    pred_df <- get_predictions_df(df, fit, technique, kind, 0.1)
     names(pred_df)[match('fit', names(pred_df))] <- target_col
     
     data.frames <- append(data.frames, list(pred_df))
