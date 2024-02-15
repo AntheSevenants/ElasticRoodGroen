a172cffc8d302309b8ca9a1528be8a4123eb19a1
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2024-02-15 11:27:07 +0100

    Change other_columns class to character instead of list

--- 1-2 Elastic Net.R
+++ 1-2 Elastic Net.R
@@ -22,8 +22,8 @@ df$country <- as.factor(df$country)
 ds <- dataset(df=df,
               response_variable_column="order",
               to_binary_columns=c("participle"),
-              other_columns=list("country", "separable", "adjectiveness", 
-                                 "priming_rate", "logfreq", "edited"))
+              other_columns=c("country", "separable", "adjectiveness", 
+                              "priming_rate", "logfreq", "edited"))
 
 # Get the list of features
 # In our case, this is the list of participles
