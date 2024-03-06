761713517e0c7a442e4c5e283b5bbf343c384e9a
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2024-03-06 11:26:44 +0100

    Add parallelised clusgap measure

---
+++ snippets/clusgap.R
@@ -0,0 +1,128 @@
+# from https://github.com/pimentel/cluster/blob/master/R/clusGap.R
+
+clusGap <- function (x, FUNcluster, K.max, B = 100, verbose = interactive(),
+                     do_parallel = FALSE, ...)
+{
+  stopifnot(is.function(FUNcluster), length(dim(x)) == 2, K.max >= 2,
+            (n <- nrow(x)) >= 1, (p <- ncol(x)) >= 1)
+  if(B != (B. <- as.integer(B)) || (B <- B.) <= 0)
+    stop("'B' has to be a positive integer")
+  
+  apply_fun <- lapply
+  
+  if (do_parallel)
+  {
+    apply_fun <- parallel::mclapply
+  }
+  
+  if(is.data.frame(x))
+    x <- as.matrix(x)
+  ii <- seq_len(n)
+  W.k <- function(X, kk) {
+    clus <- if(kk > 1) FUNcluster(X, kk, ...)$cluster else rep.int(1L, nrow(X))
+    ##                 ---------- =  =       -------- kmeans() has 'cluster'; pam() 'clustering'
+    0.5* sum(vapply(split(ii, clus),
+                    function(I) { xs <- X[I,, drop=FALSE]
+                    sum(dist(xs)/nrow(xs)) }, 0.))
+  }
+  
+  E.logW <- SE.sim <- numeric(K.max)
+  
+  if(verbose) cat("Clustering k = 1,2,..., K.max (= ",K.max,"): .. ", sep='')
+  
+  logW <- unlist(apply_fun(1:K.max, function(k) log(W.k(x, k))))
+  
+  if(verbose) cat("done\n")
+  
+  ## Scale 'x' into "hypercube" -- we later fill with H0-generated data
+  xs <- scale(x, center=TRUE, scale=FALSE)
+  m.x <- rep(attr(xs,"scaled:center"), each = n)# for back transforming
+  V.sx <- svd(xs)$v
+  rng.x1 <- apply(xs %*% V.sx, # = transformed(x)
+                  2, range)
+  
+  if(verbose) cat("Bootstrapping, b = 1,2,..., B (= ", B,
+                  ")  [one \".\" per sample]:\n", sep="")
+  
+  logWksList <- apply_fun(1:B,
+                          function(b)
+                          {
+                            ## Generate "H0"-data as "parametric bootstrap sample" :
+                            z1 <- apply(rng.x1, 2,
+                                        function(M, nn) runif(nn, min=M[1], max=M[2]),
+                                        nn=n)
+                            z <- tcrossprod(z1, V.sx) + m.x # back transformed
+                            curLogWks <- unlist(lapply(1:K.max, function(k) log(W.k(z, k))))
+                            if(verbose && !do_parallel) cat(".", if(b %% 50 == 0) paste(b,"\n"))
+                            
+                            curLogWks
+                          })
+  
+  logWks <- matrix(unlist(logWksList),
+                   byrow = TRUE,
+                   nrow = B)
+  
+  if(verbose && (B %% 50 != 0)) cat("",B,"\n")
+  E.logW <- colMeans(logWks)
+  SE.sim <- sqrt((1 + 1/B) * apply(logWks, 2, var))
+  structure(class = "clusGap",
+            list(Tab = cbind(logW, E.logW, gap = E.logW - logW, SE.sim),
+                 ## K.max == nrow(T)
+                 n = n, B = B, FUNcluster=FUNcluster))
+}
+
+## lga/R/gap.R   --- has for Tibshirani et al (2001):
+## ElogWks[k,] <- c(mean(BootOutput), sqrt(var(BootOutput)*(1+1/B)))
+## GAP[k] <- ElogWks[k,1] - logWks[k]
+## if (k > 1)
+##     if(GAP[k-1] >= GAP[k]-ElogWks[k,2] & !doall)
+##         finished <- TRUE
+##  so they effectively only look for the *first* (local) maximum which ..
+## MM: <==> diff(GAP) = GAP[k] - GAP[k-1] <= +SE.sim[k]
+
+
+## criteria.DandF() -- Dudoit and Fridlyand (2002)
+## ---------------- looks at the *global* maximum and then to the left..
+## y <- x$data
+## crit <- diff(y[which.max(y[,"Gap"]), c("Sks", "Gap")])
+## nclust <- min(which(y[,"Gap"] > crit))
+## return(ifelse(nclust == nrow(y), NA, nclust))
+
+maxSE <- function(f, SE.f,
+                  method = c("firstSEmax", "Tibs2001SEmax",
+                             "globalSEmax", "firstmax", "globalmax"),
+                  SE.factor = 1)
+{
+  method <- match.arg(method)
+  stopifnot((K <- length(f)) >= 1, K == length(SE.f), SE.f >= 0, SE.factor >= 0)
+  fSE <- SE.factor * SE.f
+  switch(method,
+         "firstmax" = { ## the first local maximum  (== firstSEmax with SE.factor == 0)
+           decr <- (dg <- diff(f)) <= 0 # length K-1
+           if(any(decr)) which.max(decr) else K # the first TRUE, or K
+         },
+         "globalmax" = {
+           which.max(f)
+         },
+         "Tibs2001SEmax" = { ## The one Tibshirani et al (2001) proposed:
+           ## "the smallest k such that f(k) >= f(k+1) - s_{k+1}"
+           g.s <- f - fSE
+           if(any(mp <- f[-K] >= g.s[-1])) which.max(mp) else K
+         },
+         "firstSEmax" = { ## M.Maechler(2012): rather ..
+           ## look at the first *local* maximum and then to the left ..:
+           decr <- (dg <- diff(f)) <= 0 # length K-1
+           nc <- if(any(decr)) which.max(decr) else K # the first TRUE, or K
+           if(any(mp <- f[seq_len(nc - 1)] >= f[nc] - fSE[nc]))
+             which(mp)[1]
+           else nc
+         },
+         "globalSEmax" = { ## Dudoit and Fridlyand (2002) *thought* Tibshirani proposed..
+           ## in 'lga', see criteria.DandF():
+           ## looks at the *global* maximum and then to the left..
+           nc <- which.max(f)
+           if(any(mp <- f[seq_len(nc - 1)] >= f[nc] - fSE[nc]))
+             which(mp)[1]
+           else nc
+         })
+}
