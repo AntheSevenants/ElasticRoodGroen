ecdb0387a4b905b57ca6c60031f605616f4f7e37
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2024-04-10 11:51:23 +0200

    Add comparison with Jelke Bloem's data

--- 1-7 Comparison.R
+++ 1-7 Comparison.R
@@ -1,12 +1,22 @@
-df_net <- read.csv("output/RoodGroenAnthe_coefficients.csv")
+df_net <- read.csv("output/RoodGroenAnthe_coefficients_infused.csv")
 df_llr <- read.csv("data/DeSutterLLR.csv")
+df_oddsr <- read.csv("data/BloemOddsHebben.csv")
 
-df_comp <- merge(x=df_net, y=df_llr,
+# De Sutter
+df_ds <- merge(x=df_net, y=df_llr,
             by.x="feature", by.y="participle", all.y=TRUE)
 # Zero coefficients in Elastic Net, 
-df_comp[!is.na(df_comp$coefficient) & df_comp$coefficient == 0,]
-df_comp[is.na(df_comp$coefficient),]
+df_ds[!is.na(df_ds$coefficient) & df_ds$coefficient == 0,]
+df_ds[is.na(df_ds$coefficient),]
 
 # TODO: make plot better (read: add red and green colours)
-plot(df_comp$llr, df_comp$coefficient)
-cor.test(df_comp$coefficient, df_comp$llr) # of course, the correlation is true
+plot(df_ds$llr, df_ds$coefficient)
+cor.test(df_ds$coefficient, df_ds$llr) # of course, the correlation is true
+
+# Bloem
+df_bloem <- merge(x=df_net, y=df_oddsr, by.x="lemma", by.y="lemma", all.y=T)
+
+plot(df_bloem$oddsr, df_bloem$coefficient)
+
+cor.test(df_bloem$coefficient, df_bloem$oddsr)
+
