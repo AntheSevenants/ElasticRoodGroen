aeaf7150e3856866948f92b73e4d0077849e75ae
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2024-04-10 11:55:15 +0200

    Combine Cornetto and semcor data frames (from Python output)

--- 1-8 Semantic analysis.R
+++ 1-8 Semantic analysis.R
@@ -1,60 +1,11 @@
-library(xml2)
-library(parallel)
+semcor <- read.csv("output/RoodGroenAnthe_coefficients_semantics.csv")
+cornetto <- read.csv("output/cornetto_info.csv")
 
-# First, load all semcor files which match the verb type
-semcor_path <- "data/dutsemcor/*.v.xml"
-semcor <- data.frame(path = Sys.glob(semcor_path, dirmark = FALSE))
-# Then, extract the verb lemma from the filename
-semcor$lemma <- gsub(".*/(.*).v.xml", "\\1", semcor$path, ignore.case=T)
+df <- merge(semcor, cornetto, by.x = "dominant_sense", by.y = "sense")
 
-# Read the elastic net output, with lemmas
-df <- read.csv("output/RoodGroenAnthe_coefficients_infused.csv")
+cornetto$dominant_sense[duplicated(cornetto$dominant_sense)]
+semcor$dominant_sense[duplicated(semcor$dominant_sense)]
+semcor$lemma[duplicated(semcor$lemma)]
+df$lemma[duplicated(df$lemma)]
 
-# Only keep lemmas which have a DutchSemCor entry
-semcor <- merge(x=df, y=semcor, by="lemma")
-
-# Yes, parsing XML with regex is bad
-# But the XML files are very simple, and some DutchSemCor files are HUGE
-# so: this is a memory safe solution. don't @ me
-get_senses_memory_safe <- function(filepath) {
-  senses <- c()
-  con = file(filepath, "r")
-  while (TRUE) {
-    line = readLines(con, n = 1, warn = FALSE)
-    if (length(line) == 0) {
-      break
-    }
-    sense <- gsub(".*sense=\"(.*?)\".*", "\\1", line)
-    if (substring(sense, 1, 1) == "<") {
-      next
-    } else {
-      senses <- append(senses, sense)
-    }
-  }
-  
-  close(con)
-  
-  topmost_sense <- senses %>% table %>% sort %>% names %>% first
-  
-  return(topmost_sense)
-}
-
-get_topmost_sense <- function(senses) {
-  topmost_sense <- senses %>% table %>% sort %>% names %>% first
-  
-  return(topmost_sense)
-}
-
-get_topmost_sense_from_path <- function(filepath) {
-  senses <- get_senses_memory_safe(filepath)
-  topmost_sense <- get_topmost_sense(senses)
-  
-  return(topmost_sense)
-}
-
-semcor$path %>% lapply(get_topmost_sense_from_path) %>% unlist()
-
-read_xml(semcor$path) %>% xml_find_all("/tokens/token") %>% xml_attr("sense")
-
-employee_data <- read_xml("data/dutsemcor/afspelen.v.xml")
-xml_find_all(employee_data, "/tokens/token") %>% xml_attr("sense") %>% table
+lm(coefficient ~ morpho_type + conj_type + valency + transitivity + sclass + reflexive + subject + sem_type, data=df) %>% summary
