3a963f515608169c7264447d3d7647361001620b
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2024-04-10 11:50:09 +0200

    Remove all junk we're not using anymore in the distributional semantics script

--- 1-4 Distributional Semantics FULL.R
+++ 1-4 Distributional Semantics FULL.R
@@ -6,8 +6,6 @@ options(scipen=999)
 
 # Use DistributionalSemanticsR scripts
 source("DistributionalSemanticsR/VectorSpace.R")
-source("DistributionalSemanticsR/DimensionReductionFactory.R")
-source("ElasticToolsR/MetaFile.R")
 
 # Read coefficients output
 df <- read.csv("output/RoodGroenAnthe_coefficients_infused.csv")
@@ -67,33 +65,6 @@ distributional_coords_non_zero <-
 distributional_coords_outside_sd <-
   distributional_coords[-within_sd_indices,]
 
-dist_mat <- function(coords) {
-  mat <- dist(coords, method="euclidean", diag=T, upper=T)
-  mat <- as.matrix(mat)
-  return(mat)
-}
-
-do_pca <- function(coords, dims=2) {
-  clustering <- prcomp(coords, scale = FALSE)$x[,1:dims]
-  
-  return(clustering)
-}
-
-do_mds <- function(coords, dims=2) {
-  
-}
-
-do_tsne <- function(coords, dims=2) {
-  clustering <- Rtsne(coords, is_distance=TRUE, dims=dims)$Y %>% as.matrix()
-  rownames(clustering) <- rownames(coords)
-  
-  return(clustering)
-}
-
-do_umap <- function(coords, dims=2) {
-  umap(coords, n_components=dims)$layout
-}
-
 get_color <- function(df, mat) {
   origin <- data.frame(feature = rownames(mat))
   
@@ -112,36 +83,6 @@ do_plot <- function(mat, colors=NA) {
   
   plot(mat, col = colors)
 }
-
-do_3d_plot <- function(mat, name) {
-  colors <- get_color(df, mat)
-  
-  plot3d(mat[,1], mat[,2], mat[,3], col=colors)
-  
-  htmlwidgets::saveWidget(rglwidget(width = 520, height = 520), 
-                          file = paste0("widgets/", name, ".html"),
-                          libdir = "libs",
-                          selfcontained = FALSE
-  )
-}
-
-get_coords("non_zero") %>% dist_mat() %>% do_pca(3) %>% do_3d_plot("full_pca_non_zero")
-get_coords("non_zero") %>% dist_mat() %>% do_tsne(3) %>% do_3d_plot("full_tsne_non_zero")
-get_coords("non_zero") %>% do_umap(3) %>% do_3d_plot("full_umap_non_zero")
-
-get_coords("all") %>% do_pca(2) %>% do_plot
-get_coords("non_zero") %>% do_pca(2) %>% do_plot
-get_coords("outside_sd") %>% do_pca(2) %>% do_plot
-
-get_coords("all") %>% dist_mat() %>% do_tsne(2) %>% do_plot
-get_coords("non_zero") %>% dist_mat() %>% do_tsne(2) %>% do_plot
-get_coords("outside_sd") %>% dist_mat() %>% do_tsne(2) %>% do_plot
-
-get_coords("all") %>% do_umap(2) %>% do_plot
-get_coords("non_zero") %>% do_umap(2) %>% do_plot
-get_coords("outside_sd") %>% do_umap(2) %>% do_plot
-
-
 
 get_coords <- function(mode) {
   # Mode can be "all", "non_zero", "outside_sd"
@@ -165,16 +106,10 @@ n_cluster_search
 
 # We look for the cluster count where adding a cluster doesn't improve
 # the model anymore
-# So, I filter for the first non-increasing cluster, then subtract one
-n_cluster <- n_cluster_search %>% filter(!AddCluster) %>% first %>% .$Cluster - 1
-# n_cluster <- 13
-# The output is, for some reason, not deterministic, even with a seed
-# It hovers from 13 to 17
-# I picked 13, since that's the value I got first
+# So, I filter for the first non-increasing cluster (AddCluster = FALSE)
+n_cluster <- n_cluster_search %>% filter(!AddCluster) %>% first %>% .$Cluster
 
 PlotHartigan(n_cluster_search)
-
-n_cluster_search[order()]
 
 # Define the clustering function
 do_clustering <- function(df,
@@ -214,96 +149,6 @@ do_clustering <- function(df,
 df <- do_clustering(df, get_coords("all"), "kmeans", "all", "full")
 df <- do_clustering(df, get_coords("non_zero"), "kmeans", "non_zero", "full")
 df <- do_clustering(df, get_coords("outside_sd"), "kmeans", "outside_sd", "full")
-
-df <- do_clustering(df, get_coords("all") %>% dist_mat() %>% do_pca(2), "kmeans", "all", "pca2d")
-df <- do_clustering(df, get_coords("non_zero") %>% dist_mat() %>% do_pca(2), "kmeans", "non_zero", "pca2d")
-df <- do_clustering(df, get_coords("outside_sd") %>% dist_mat() %>% do_pca(2), "kmeans", "outside_sd", "pca2d")
-
-df <- do_clustering(df, get_coords("all") %>% dist_mat() %>% do_pca(10), "kmeans", "all", "pca10d")
-df <- do_clustering(df, get_coords("non_zero") %>% dist_mat() %>% do_pca(10), "kmeans", "non_zero", "pca10d")
-df <- do_clustering(df, get_coords("outside_sd") %>% dist_mat() %>% do_pca(10), "kmeans", "outside_sd", "pca10d")
-
-df <- do_clustering(df, get_coords("all") %>% dist_mat() %>% do_tsne(2), "kmeans", "all", "tsne2d")
-df <- do_clustering(df, get_coords("non_zero") %>% dist_mat() %>% do_tsne(2), "kmeans", "non_zero", "tsne2d")
-df <- do_clustering(df, get_coords("outside_sd") %>% dist_mat() %>% do_tsne(2), "kmeans", "outside_sd", "tsne2d")
-
-df <- do_clustering(df, get_coords("all") %>% dist_mat() %>% do_tsne(3), "kmeans", "all", "tsne3d")
-df <- do_clustering(df, get_coords("non_zero") %>% dist_mat() %>% do_tsne(3), "kmeans", "non_zero", "tsne3d")
-df <- do_clustering(df, get_coords("outside_sd") %>% dist_mat() %>% do_tsne(3), "kmeans", "outside_sd", "tsne3d")
-
-
-
-# Correlation analysis
-do_correlation_analysis <- function(df, exclude_features, coords) {
-  df <- df[!(df$feature %in% exclude_features),]
-  
-  dimensions <- dim(coords)[2]
-  
-  print(df$feature %>% length)
-  print(coords %>% rownames %>% length)
-  
-  p_values <- double()
-  correlations <- double()
-  
-  for (index in 1:dimensions) {
-    test <- cor.test(coords[,index], df$coefficient)
-    
-    p_values <- append(p_values, test$p.value)
-    correlations <- append(correlations, test$estimate)
-  }
-  
-  return_df <- data.frame(p = p_values,
-                          correlation = correlations)
-  
-  return (return_df)
-}
-
-do_bonferroni <- function(df) {
-  df$bon_sig <- df$p <= 0.05 / dim(df)[1]
-  
-  return(df)
-}
-
-# Trying to find correlation between dimensions and red-green coefficients
-# I'm even applying Bonferroni correction, but too it still looks like fishing
-# for spurious correlations (which is not in the scientific spirit)
-
-correlation_all_df <- 
-  do_correlation_analysis(df, no_vector_features, distributional_coords_all) %>%
-  do_bonferroni()
-
-correlation_non_zero_df <-
-  do_correlation_analysis(df, c(zero_features, no_vector_features),
-                          distributional_coords_non_zero) %>%
-  do_bonferroni()
-
-correlation_outside_sd_df <-
-  do_correlation_analysis(df, c(within_sd_features, no_vector_features),
-                          distributional_coords_outside_sd) %>%
-  do_bonferroni()
-
-# Add vector dimensions to the dataframe
-dimensions <- dim(distributional_coords)[2]
-# df$id <- 1:nrow(df)
-for (index in 1:dimensions) {
-  df[[paste0("dim_", index)]] <- distributional_coords[,index]
-  
-  # merger_df <- distributional_coords_all[,index] %>% as.data.frame()
-  # features <- rownames(merger_df)
-  # merger_df$feature <- features
-  # colnames(merger_df) <- c(paste0("dim_", index), "feature")
-  # 
-  # df <- merge(df, merger_df, by="feature", all.x=TRUE)
-}
-
-
-# Reinstate the ordering
-df <- df[order(df$id), ]
-# Throw away id column
-df$id <- NULL
-
-dim(distributional_coords_all)
-distributional_coords_all[,1]
 
 write.csv(df, "output/RoodGroenAnthe_coefficients_infused_vectors.csv", 
           row.names=FALSE)
