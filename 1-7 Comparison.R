fb787f5fbccccdf22b444577c6ce04d16d08b9d5
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2024-04-15 22:16:57 +0200

    Update comparison scripts so they can be used in the paper

--- 1-7 Comparison.R
+++ 1-7 Comparison.R
@@ -6,12 +6,24 @@ df_oddsr <- read.csv("data/BloemOddsHebben.csv")
 df_ds <- merge(x=df_net, y=df_llr,
             by.x="feature", by.y="participle", all.y=TRUE)
 # Zero coefficients in Elastic Net, 
-df_ds[!is.na(df_ds$coefficient) & df_ds$coefficient == 0,]
-df_ds[is.na(df_ds$coefficient),]
+ds_zero <- df_ds[!is.na(df_ds$coefficient) & df_ds$coefficient == 0,]
+
+ds_na <- df_ds[is.na(df_ds$coefficient),]
 
 # TODO: make plot better (read: add red and green colours)
-plot(df_ds$llr, df_ds$coefficient)
-cor.test(df_ds$coefficient, df_ds$llr) # of course, the correlation is true
+quadrant_plot <- function(x, y, xlab=NA, ylab=NA, goffset=0, roffset=0) {
+  par(mar=c(4, 4, 1, 1))
+  
+  plot(x, y, xlab=xlab, ylab=ylab)
+  lines(x=c(0,0), y=c(120, 0), col="red")
+  lines(x=c(0,0), y=c(0, -120), col="green")
+  lines(x=c(-120,0), y=c(0,0), col="green")
+  lines(x=c(0,120), y=c(0,0), col="red")
+}
+
+quadrant_plot(df_ds$llr, df_ds$coefficient, "Log likelihood ratio", "Elastic net coefficient", -1, -1.7)
+
+ds_cor <- cor.test(df_ds$coefficient, df_ds$llr) # of course, the correlation is true
 
 # Bloem
 df_bloem <- merge(x=df_net, y=df_oddsr, by.x="lemma", by.y="lemma", all.y=T)
