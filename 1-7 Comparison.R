a348b6f76069f4b9b6f701d430bbdc5bbc15b47c
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2024-04-16 16:37:50 +0200

    Change Bloem data source and use logit for comparison

--- 1-7 Comparison.R
+++ 1-7 Comparison.R
@@ -1,6 +1,10 @@
 df_net <- read.csv("output/RoodGroenAnthe_coefficients_infused.csv")
 df_llr <- read.csv("data/DeSutterLLR.csv")
-df_oddsr <- read.csv("data/BloemOddsHebben.csv")
+
+# Read Bloem's values (asc), then convert to logit
+df_oddsr <- read.csv("data/collostruct7_wiki_asc.txt")
+df_oddsr$logit <- log(df_oddsr$Unconditional.MLE)
+
 
 # De Sutter
 df_ds <- merge(x=df_net, y=df_llr,
@@ -26,12 +30,14 @@ quadrant_plot <- function(x, y, xlab=NA, ylab=NA, goffset=0, roffset=0) {
 ds_cor <- cor.test(df_ds$coefficient, df_ds$llr) # of course, the correlation is true
 
 # Bloem
-df_bloem <- merge(x=df_net, y=df_oddsr, by.x="lemma", by.y="lemma", all.y=T)
+df_bloem <- merge(x=df_net, y=df_oddsr, by.x="lemma", by.y="Verb", all.y=T)
 df_bloem <- df_bloem[df_bloem$lemma != "zeggen",]
 
 bloem_zero <- df_bloem[!is.na(df_bloem$coefficient) & df_bloem$coefficient == 0,]
 bloem_na <- df_bloem[is.na(df_bloem$coefficient),]
 
-bloem_cor <- cor.test(df_bloem$coefficient, df_bloem$oddsr)
+df_bloem_nna <- df_bloem[!is.na(df_bloem$coefficient) & df_bloem$logit != Inf,]
+bloem_cor <- cor.test(df_bloem_nna$coefficient, df_bloem_nna$logit)
 
-#quadrant_plot(df_bloem$oddsr, df_bloem$coefficient, "Odds ratio", "Elastic net coefficient", -1, -1.7)
+#quadrant_plot(df_bloem_nna$logit, df_bloem_nna$coefficient, "Odds ratio", "Elastic net coefficient", -1, -1.7)
+
